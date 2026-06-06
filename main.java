4349b1951fc386bac8d753757b6c643bbbfb614c
KabitaSubedi-1 <kabbusubedi22@gmail.com>
2026-06-06 16:37:53 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+
+        String name = "Kreetan";
+        int year = 2026;
+
+        System.out.println("Name: " + name);
+        System.out.println("Year: " + year);
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum of " + a + " and " + b + " is " + sum);
+
+        System.out.println("Java program executed successfully.");
+    }
+}
